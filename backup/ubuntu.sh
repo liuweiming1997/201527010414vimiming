f26af5aa60a5f792ef3b089e6ab2f24d4f4e725a
vimi <vimiming@gmail.com>
2019-03-12 14:59:45 +0800

    install docker-compose

--- backup/ubuntu.sh
+++ backup/ubuntu.sh
@@ -50,7 +50,7 @@ alias github="git-open"
 curl -fsSL https://get.docker.com/ | sh
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # install golang
